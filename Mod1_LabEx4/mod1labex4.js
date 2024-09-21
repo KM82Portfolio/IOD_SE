793ffdb4d1a8fa10c098257e7583d3207fdf8b1c
KM82Portfolio <km82port@proton.me>
2024-09-21 19:06:46 +0800

    mod1labex4.js

---
+++ Mod1_LabEx4/mod1labex4.js
@@ -0,0 +1,17 @@
+function sum(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+console.log("5+2 = ",sum(5,2));
+console.log("5-2 = ",subtract(2,5));
+console.log("5*2 = ",multiply(5,2));
+console.log("5/2 = ",divide(5,2));
