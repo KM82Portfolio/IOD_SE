7f95be679e82cff27d616877d3304d3a3dbae6ef
KM82Portfolio <km82port@proton.me>
2024-09-23 19:35:39 +0800

    Module 1 Lab Exercise 6

---
+++ Mod1_LabEx6/mod1labex6.js
@@ -0,0 +1,15 @@
+const book = {
+    "title":"Bad jokes",
+    "description":"Book of really bad jokes",
+    "author":"Who else?",
+    "totalpages":1
+};
+
+console.log("Book Title : ",book.title);
+console.log("Book Description : ",book.description);
+console.log("Book Author : ",book.author);
+console.log("Book Total Number of Pages : ",book.totalpages);
+
+book.description = "The jokes could get worse.";
+
+console.log("New Book Description : ",book.description);
