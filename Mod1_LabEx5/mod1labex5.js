5470e19e4ff204f5be3a3ca213a1b64391542798
KM82Portfolio <km82port@proton.me>
2024-09-28 11:53:49 +0800

    Quick Updates

--- Mod1_LabEx5/mod1labex5.js
+++ Mod1_LabEx5/mod1labex5.js
@@ -1,4 +1,24 @@
 let arr1 = new Array();
+arr1 = [1,2,3,4,5.5];
+
+console.log("Original Array is",arr1);
+
+arr1[0]=88;
+arr1[3]=16;
+console.log("Modified Array with elements at position 1 and 4 replaced is",arr1);
+
+arr1.unshift(888);
+console.log("Modified Array with element added at the start is",arr1);
+console.log("Getting string property".length);
+
+//arr3=arr1.pop();
+//console.log("Modified Array with last element removed is",arr1);
+
+arr1.pop();
+console.log("Modified Array with last element removed is",arr1);
+
+/*
+let arr1 = new Array();
 arr1 = [1,2,"three","four",5.5];
 
 console.log("Original Array is",arr1);
@@ -7,5 +27,13 @@ arr1[0]=88;
 arr1[3]="sixteen";
 console.log("Modified Array with elements at position 1 and 4 replaced is",arr1);
 
-arr2=arr1.pop();
+arr1.unshift("Root");
+console.log("Modified Array with element added at the start is".arr1);
+
+arr3=arr1.pop();
 console.log("Modified Array with last element removed is",arr1);
+
+const array1 = [1, 2, 3];
+
+console.log("Test",array1.unshift(4, 5));
+*/
