13bf027484b1c8fbb2bde6d4f96fb790e5c4a6a8
KM82Portfolio <km82port@proton.me>
2024-09-28 11:18:06 +0800

    Module 1 Lab Exercise 4 Update

--- Mod1_LabEx4/mod1labex4.js
+++ Mod1_LabEx4/mod1labex4.js
@@ -10,11 +10,15 @@ function multiply(a,b){
 function divide(a,b){
     return a/b;
 }
+function printName(name){
+    console.log("Hello ",name);
+}
 
 console.log("5+2 = ",sum(5,2));
 console.log("5-2 = ",subtract(2,5));
 console.log("5*2 = ",multiply(5,2));
-console.log("5/2 = ",divide(5,2));
+console.log("5/2 = ",divide(5,2));cd
+printName("KenM");
 
 /*
 module.export.sum1 = function(a,b){
