52cd21731c25539d668e1625fe92f4ffbc9fee19
KM82Portfolio <km82port@proton.me>
2024-09-28 13:03:50 +0800

    Module 1 Lab Exercise 6 Updates

--- Mod1_LabEx6/mod1labex6.js
+++ Mod1_LabEx6/mod1labex6.js
@@ -1,19 +1,36 @@
-const book = {
-    "title":"Bad jokes",
-    "description":"Book of really bad jokes",
-    "author":"Who else?",
-    "totalpages":1
-};
+let book = require("./book.json");
+console.log(book);
+//let book = JSON.parse(rd);
 
 console.log("Book Title : ",book.title);
 console.log("Book Description : ",book.description);
 console.log("Book Author : ",book.author);
 console.log("Book Total Number of Pages : ",book.totalpages);
 
-book.description = "The jokes could get worse.";
+//function to rewrite obj properties
+function reWriter(obj_prop,replacement_text){
+    book[obj_prop] = replacement_text; //within
+}
 
-console.log("New Book Description : ",book.description);
+reWriter("description","The jokes could get worse.");//call function
+console.log("New Book Description : ",book.description);//print output to log
 
-const rawdata = '{"title":"Bad jokes","description":"Book of really bad jokes", "author":"Who else?","totalpages":1}';
-let book1 = JSON.parse(rawdata);
-console.log("Parsed JSON title : ",book1.title);
+//objection property access test
+console.log("This book has ",book.Chapters.length," chapters");
+console.log("The title of the 4th Chapter is",book.Chapters[3]);
+
+//JSON parse test
+//const rawdata = '{"title":"Bad jokes","description":"Book of really bad jokes", "author":"Who else?","totalpages":1}';
+//let book1 = JSON.parse(rawdata);
+//console.log("Parsed JSON title : ",book1.title);
+
+
+
+
+//function to rewrite obj properties
+function reWriter1(obj,obj_prop,replacement_text){
+    obj[obj_prop] = replacement_text; //within
+}
+
+reWriter1("book","title","New Title");
+console.log("Pass object test",book.title);
