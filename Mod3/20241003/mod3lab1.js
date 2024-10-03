03e6578b42fd58a91f445a1aaadae6ebe92f5751
KM82Portfolio <km82port@proton.me>
2024-10-03 21:29:56 +0800

    3 Oct 2024 Lesson

---
+++ Mod3/20241003/mod3lab1.js
@@ -0,0 +1,27 @@
+const a = "123";
+console.log(parseInt(a));
+console.log(typeof parseInt(a));
+
+const b = "text";
+console.log(parseInt(b));
+console.log(typeof parseInt(b)); //NaN is an enumeration?
+
+var _ = require('lodash'); //testing lodash for class exercise
+c = ["123","456"];
+d = _.map(c,x=>parseInt(x));
+for(let i=0;i<d.length;i++){
+    console.log(d[i],typeof d[i]);
+}
+
+/*
+console.log(typeof undefined); // undefined
+console.log(typeof 0); // number
+console.log(typeof 10n); // bigint
+console.log(typeof true); // boolean
+console.log(typeof 'text'); // string
+console.log(typeof Symbol('id')) // symbol
+console.log(typeof Math); // object
+console.log(typeof null); // object
+console.log(typeof console.log); // function
+*/
+
