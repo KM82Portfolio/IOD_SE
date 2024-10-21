19937140e8af263420e780af573eab861359b165
KM82Portfolio <km82port@proton.me>
2024-10-21 20:57:44 +0800

    Mini Project 1 Updates

--- Mini_Project_1/js/test.js
+++ Mini_Project_1/js/test.js
@@ -32,3 +32,23 @@ console.log(testArr);
 
 //How to remove row in 2d array in javascript
 //https://stackoverflow.com/questions/29070924/how-to-remove-row-in-2d-array-in-javascript
+
+let jData = [
+    { name: "GeeksforGeeks", est: 2009 },
+    { name: "Google", est: 1998 },
+    { name: "Microsoft", est: 1975 }
+];
+console.log(jData)
+let res = jData.reduce((acc, curr) => {
+    let ind = acc.findIndex((item) => item.est > curr.est);
+    if (ind     === -1) ind = acc.length;
+    acc.splice(ind, 0, curr);
+    return acc;
+}, []);
+console.log(res);
+
+var transactionTable = '{"transactionTable":[]}'
+var parse_obj = JSON.parse(transactionTable);
+parse_obj['transactionTable'].push({"teamId":"4","status":"pending"});
+transactionTable = JSON.stringify(parse_obj);
+console.log(transactionTable);
