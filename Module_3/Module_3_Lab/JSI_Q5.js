c2d8233c1e24d9fe2b5bf760dfb21f8dc7d762be
KM82Portfolio <km82port@proton.me>
2024-10-27 00:48:56 +0800

    Module 3 Lab Updates

--- Module_3/Module_3_Lab/JSI_Q5.js
+++ Module_3/Module_3_Lab/JSI_Q5.js
@@ -20,23 +20,27 @@ function currencyAddition(float1, float2){
 console.log(currencyAddition(123.423423424,123.534345345))
 
 function currencyOperation(float1, float2, operation, numDecimals){
-    switch(operation){
-        case '+':
-            let plus = float1 + float2;
-            return plus.toFixed(numDecimals);
-            break;
-        case '-':
-            let minus float1 - float2;
-            return minus.toFixed(numDecimals);
-            break;
-        case '/':
-            let divide = float1 / float2;
-            return divide.toFixed(numDecimals);
-            break;
-        case '*':
-            let multiply = float1 * float2;
-            return multiply.toFixed(numDecimals);
-            break;
-    }
+    if(typeof float1==Number && typeof float2==Number){ //prevent string addition problems
+        switch(operation){
+            case '+':
+                let plus = float1 + float2;
+                return plus.toFixed(numDecimals);
+                break;
+            case '-':
+                let minus float1 - float2;
+                return minus.toFixed(numDecimals);
+                break;
+            case '/':
+                let divide = float1 / float2;
+                return divide.toFixed(numDecimals);
+                break;
+            case '*':
+                let multiply = float1 * float2;
+                return multiply.toFixed(numDecimals);
+                break;
+            default:
+                console.log(`Check operator selected`)//if caller sends something apart from the above 4 operators
+        }
+    }else{console.log(`Check number data types & try again`)}
 }
 console.log(currencyAddition(123.123123123,123.123123123,'+'))
--- Module_3/Module_3_Lab/JSI_Q8.js
+++ Module_3/Module_3_Lab/JSI_Q8.js
@@ -4,3 +4,30 @@ phoneBookABC.set('Barry', '0433221117')
 phoneBookABC.set('Caroline', '0455221182')
 
 console.log(phoneBookABC)
+
+// Q8a) Create a new phoneBookDEF Map to store names beginning with D, E or F
+// Q8b) Initialise the contents of phoneBookDEF by passing in an array of keys/values
+// Meaning LITERALLY a Map() of keys starting with ''D'/'E'/'F' like so?
+const phoneBookDEF = new Map()
+phoneBookDEF.set('Daniel', '123456') 
+phoneBookDEF.set('Ellen', '789101') 
+phoneBookDEF.set('Frank', '121314')
+
+// Q8c) Update the phone number for Caroline
+phoneBookABC.set('Caroline', '87654321') //can update values of existing keys using Map.set()
+
+// Q8d) Write a function printPhoneBook(contacts) that prints the names and phone numbers in the given Map
+// Guessing contacts is the Map object that will be taken as an argument to the function
+function printPhoneBook(contacts){
+    return contacts.forEach((k,v)=>console.log(`Name : ${k}, Phone Number : ${v}`))
+}
+printPhoneBook(phoneBookABC)
+
+console.log(`
+`)
+
+// Q8e) Combine the contents of the two individual Maps into a single phoneBook Map
+const phoneBookABCDEF = new Map([...phoneBookABC,...phoneBookDEF]);
+printPhoneBook(phoneBookABCDEF);
+console.log(`
+`)
