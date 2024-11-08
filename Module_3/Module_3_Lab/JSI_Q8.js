bcec9dbfe6c47c98c1e31f416ee52a7dad049bcd
KM82Portfolio <km82port@proton.me>
2024-11-08 22:03:18 +0800

    JS Intermediate Lab Updates

--- Module_3/Module_3_Lab/JSI_Q8.js
+++ Module_3/Module_3_Lab/JSI_Q8.js
@@ -19,7 +19,7 @@ phoneBookABC.set('Caroline', '87654321') //can update values of existing keys us
 // Q8d) Write a function printPhoneBook(contacts) that prints the names and phone numbers in the given Map
 // Guessing contacts is the Map object that will be taken as an argument to the function
 function printPhoneBook(contacts){
-    return contacts.forEach((k,v)=>console.log(`Name : ${k}, Phone Number : ${v}`))
+    return contacts.forEach((k,v)=>console.log(`Name : ${v}, Phone Number : ${k}`)) 
 }
 printPhoneBook(phoneBookABC)
 
@@ -31,3 +31,33 @@ const phoneBookABCDEF = new Map([...phoneBookABC,...phoneBookDEF]);
 printPhoneBook(phoneBookABCDEF);
 console.log(`
 `)
+
+// Q8a) If asking to generate a Map() object that only accepts names starting with 'D', 'E' or 'F'
+// function mapDEF(name,phoneNum){
+//     const phoneBookDEF = new Map();
+//     if(name.startsWith('D')){phoneBookDEF.set(name,phoneNum);}
+//     // phoneBookDEF.set(name,phoneNum) ? name.startsWith('D') : {}
+//     return phoneBookDEF
+// }
+
+// mD1 = mapDEF('Daniel', '123456');
+// mD2 = mapDEF('Johnny', '654321');
+// printPhoneBook(mD1);
+// printPhoneBook(mD2);
+
+const phoneBookDEF1 = new Map()
+phoneBookDEF1.set('Daniel', '123456') 
+phoneBookDEF1.set('Johnny', '654321') 
+phoneBookDEF1.set('Frank', '121314')
+
+function mapDEF1(contacts){
+    let mpDEF1 = new Map();
+    contacts.forEach((k,v)=>{
+        if(v.startsWith('D')||v.startsWith('E')||v.startsWith('F')){
+            mpDEF1.set(k,v);
+        }
+    })
+    return mpDEF1;
+}
+console.log('phoneBookDEF1',phoneBookDEF1);
+console.log('Map Filter Test',mapDEF1(phoneBookDEF1));
