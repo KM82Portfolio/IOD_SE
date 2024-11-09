750936038032f5ff799f0911546e9d17030218ca
KM82Portfolio <km82port@proton.me>
2024-11-09 13:45:49 +0800

    JS Advancd Lab

--- Module_3/Module_3_Lab/JSA_Q9.js
+++ Module_3/Module_3_Lab/JSA_Q9.js
@@ -4,23 +4,19 @@ function randomDelay() {
     
     let randomDelayDuration = (Math.random()*(20-1)+1)*1000; //delays execution for a random amount of time (between 1 and 20 seconds)
 
-    const promiseObj = new Promise((resolve,reject)=>
+    return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             if(randomDelayDuration > 10) {
-                console.log(`setTimeout to random delay duration of ${randomDelayDuration} ms for RESOLVE condition`);
                 resolve(randomDelayDuration);
             }
-            else if(randomDelayDuration<=10) {
-                console.log(`setTimeout to random delay duration of ${randomDelayDuration} ms for REJECT condition`);
+            else {
                 reject(randomDelayDuration);
             }
         },randomDelayDuration)
-    )
-
-    return promiseObj;
+    });
 
 } 
 
 randomDelay()
-    .then((anything)=>console.log(`Random delay duration of ${anything} ms triggered`))
-    .catch((anything)=>console.log(`Random delay duration of ${anything} ms triggered`));
+    .then((response)=>console.log(`Random delay duration of ${response/1000} ms triggered`))
+    .catch((response)=>console.log(`Random delay duration of ${response/1000} ms triggered`));
