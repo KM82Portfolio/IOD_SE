bf5be65297526ff046ae06b23b6717ddcbbe6b67
KM82Portfolio <km82port@proton.me>
2024-11-21 20:46:27 +0800

    Module 4 Lab Ex 10 Refactored

--- Module_4/M4LABEX10/scripts/script.js
+++ Module_4/M4LABEX10/scripts/script.js
@@ -1,4 +1,8 @@
 // Convert this to map() but need to know how to iterate through dropdown-menu <div> <a> children
+let filteredProducts='';
+
+let catDDMContainer = document.querySelector('.cat-container');
+
 function createProductCard(pdt){
     const pdtTemplate = document.querySelector('#card-template').content.cloneNode(true);
     pdtTemplate.querySelector('.card-title').innerText = pdt.category;
@@ -24,8 +28,6 @@ function filterByCategories(productsArray,categoryToFilter){
 
 }
 
-let filteredProducts='';
-
 async function fetchProducts() {
 
     const pdtResponse = await fetch("https://fakestoreapi.com/products");
@@ -35,29 +37,35 @@ async function fetchProducts() {
     return products;//for testing accessing products outside of this async function
 }
 
+async function fetchCategories(){
+    const catResponse = await fetch("https://fakestoreapi.com/products/categories");
+    const cats = await catResponse.json();
+    console.log(cats);
+    
+    return cats;
+}
+// fetchCategories();
+
 // main() style async function that allows for eventListeners
 async function main(){
 
     let pdts = await fetchProducts();
     console.log('Sven',pdts);
-    
-    let Electronics_DDLI_Sel = document.querySelector('#Electronics_DDLI');
-    Electronics_DDLI_Sel.addEventListener('click',(e)=>{
-        console.log(e.target.innerText);
-        filteredProducts = filterByCategories(pdts,e.target.innerText);
-        console.log('Electronics_DDLI_Sel Triggered');
-    });
 
-    let Men_Clothing_DDLI_Sel = document.querySelector('#Men_Clothing_DDLI');
-    Men_Clothing_DDLI_Sel.addEventListener('click',(e)=>{
-        console.log(e.target.innerText);
-        filteredProducts = filterByCategories(pdts,e.target.innerText);
-        console.log('Men_Clothing_DDLI_Sel Triggered');
+    let cats = await fetchCategories();
+    let allFilter = document.querySelector('#all-filter');
+    allFilter.addEventListener('click',(e)=>populateProductContainer(pdts));//add button that shows everything
 
-    });
-
-    filteredProducts = filterByCategories(pdts,"electronics");
-    console.log('Filtered Products ',filteredProducts);
+    cats.forEach((cat)=>{
+        let catBtn = document.createElement('button');
+        // catBtn.className = 'dropdown-item';
+        catBtn.innerText=cat;
+        catBtn.addEventListener('click',(e)=>{
+            const filteredPDT = pdts.filter((pd)=> pd.category===cat);
+            populateProductContainer(filteredPDT);
+        })
+        catDDMContainer.appendChild(catBtn);
+      })
     
     // display filtered products if user selects to filter, else display all products
     filteredProducts!='' ? populateProductContainer(filteredProducts) : populateProductContainer(pdts);
